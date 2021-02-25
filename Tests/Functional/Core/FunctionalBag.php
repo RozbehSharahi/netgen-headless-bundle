567ade0479f0e09bd9a0967d62bc9bc690938f55
Rozbeh Chiryai Sharahi <rozbeh.sharahi@turbinekreuzberg.com>
2021-02-25 11:42:27 +0000

    Improve functional bag

--- Tests/Functional/Core/FunctionalBag.php
+++ Tests/Functional/Core/FunctionalBag.php
@@ -2,6 +2,11 @@
 
 namespace Rs\NetgenHeadless\Tests\Functional\Core;
 
+use Netgen\Layouts\API\Service\LayoutService;
+use Netgen\Layouts\API\Values\Layout\Layout;
+use Netgen\Layouts\API\Values\Layout\LayoutCreateStruct;
+use Netgen\Layouts\Exception\BadStateException;
+use Netgen\Layouts\Layout\Registry\LayoutTypeRegistry;
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -35,6 +40,50 @@ class FunctionalBag
     public function getContainer(): ContainerInterface
     {
         return $this->container;
+    }
+
+    public function getLayoutService(): LayoutService
+    {
+        return $this->container->get('netgen_layouts.api.service.layout');
+    }
+
+    public function getLayoutTypeRegistry(): LayoutTypeRegistry
+    {
+        return $this->container->get('netgen_layouts.layout.registry.layout_type');
+    }
+
+    /**
+     * @param string $title
+     * @param string $type
+     * @param string $local
+     * @return Layout
+     * @throws BadStateException
+     */
+    public function createPublishedLayout(string $title, string $type = 'layout_1', string $local = 'en'): Layout
+    {
+        return $this->getLayoutService()->publishLayout(
+            $this->getLayoutService()->createLayout(
+                $this->createLayoutCreateStruct($title, $type, $local)
+            )
+        );
+    }
+
+    /**
+     * @param string $title
+     * @param string $type
+     * @param string $local
+     * @return LayoutCreateStruct
+     */
+    protected function createLayoutCreateStruct(
+        string $title,
+        string $type = 'layout_1',
+        string $local = 'en'
+    ): LayoutCreateStruct {
+        return $this->getLayoutService()->newLayoutCreateStruct(
+            $this->getLayoutTypeRegistry()->getLayoutType($type),
+            $title,
+            $local
+        );
     }
 
 }
--- Tests/Functional/MainTest.php
+++ Tests/Functional/MainTest.php
@@ -4,8 +4,6 @@
 namespace Rs\NetgenHeadless\Tests\Functional;
 
 use Exception;
-use Netgen\Layouts\Core\Service\LayoutService;
-use Netgen\Layouts\Layout\Registry\LayoutTypeRegistry;
 use Rs\NetgenHeadless\Controller\HomeController;
 use Rs\NetgenHeadless\Tests\Functional\Core\AbstractFunctionalTest;
 use Rs\NetgenHeadless\Tests\Functional\Core\FunctionalBag;
@@ -39,23 +37,7 @@ class MainTest extends AbstractFunctionalTest
     public function testCanGetLayoutViaGraphQl()
     {
         $this->withinTransaction(function (FunctionalBag $bag) {
-            /** @var LayoutService $layoutService */
-            $layoutService = $bag->getContainer()->get('netgen_layouts.api.service.layout');
-
-            /** @var LayoutTypeRegistry $layoutTypeRegistry */
-            $layoutTypeRegistry = $bag->getContainer()->get('netgen_layouts.layout.registry.layout_type');
-
-            $layout = $layoutService->createLayout(
-                $layoutService->newLayoutCreateStruct(
-                    $layoutTypeRegistry->getLayoutType('layout_1'),
-                    'Test Layout',
-                    'en'
-                )
-            );
-
-            $layout = $layoutService->publishLayout($layout);
-
-            self::assertTrue($layout->isPublished());
+            $layout = $bag->createPublishedLayout('Some name');
 
             $response = $this->graphqlRequest($bag->getClient(), '
                 {
